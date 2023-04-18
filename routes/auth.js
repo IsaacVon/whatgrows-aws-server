6debe16239c6d2433cfd3a4e414e499e83f7c918
Isaac Householder <isaachouseholder@gmail.com>
2023-04-18 16:30:38 -0700

    fixed linting issues

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,11 @@
 const bcrypt = require('bcrypt');
-const _ = require('lodash');
-const mongoose = require("mongoose");
-const Joi = require("joi");
-const express = require("express");
+const Joi = require('joi');
+const express = require('express');
 const router = express.Router();
-const { User } = require('../models/user')  
-
-
+const { User } = require('../models/user');
 
 // Log in
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const schema = Joi.object({
     email: Joi.string().max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
@@ -17,19 +13,16 @@ router.post("/", async (req, res) => {
 
   const result = schema.validate(req.body);
 
-  if (result.error) return res.status(400).send(result.error.details[0].message);
-    let user = await User.find({ email: req.body.email });
-    user = user[0]    
-    if (!user) return res.status(400).send("Invalid email or password.");
-    const validPassword = await bcrypt.compare(req.body.password, user.password)    
-    if (!validPassword) return res.status(400).send("Invalid email or password.");
+  if (result.error)
+    return res.status(400).send(result.error.details[0].message);
+  let user = await User.find({ email: req.body.email });
+  user = user[0];
+  if (!user) return res.status(400).send('Invalid email or password.');
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-    const token = user.generateAuthToken()
-    res.send(token)    
-  
+  const token = user.generateAuthToken();
+  res.send(token);
 });
-
-
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,13 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const auth = require("../middleware/auth");
-const { User } = require("../models/user");
-const bcrypt = require("bcrypt");
-const _ = require("lodash");
-const Joi = require("joi");
-const express = require("express");
+const auth = require('../middleware/auth');
+const { User } = require('../models/user');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const Joi = require('joi');
+const express = require('express');
 const router = express.Router();
 
-
-
-
 // Register User - Input via req.body: name, email, password
-router.post("/register", async (req, res) => {
-
+router.post('/register', async (req, res) => {
   const schema = Joi.object({
     name: Joi.string().min(5).max(255).required(),
     email: Joi.string().max(255).required().email(),
@@ -28,9 +22,9 @@ router.post("/register", async (req, res) => {
   const duplicateUser = await User.find({ email: req.body.email });
 
   if (duplicateUser[0])
-    return res.status(409).send("Account already created using this email");
+    return res.status(409).send('Account already created using this email');
 
-  const user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
@@ -39,20 +33,20 @@ router.post("/register", async (req, res) => {
 
   const token = user.generateAuthToken();
   res
-    .header("x-auth-token", token)
-    .header("access-control-expose-headers", "x-auth-token")
-    .send(_.pick(newUser, ["id", "name", "email"]));
+    .header('x-auth-token', token)
+    .header('access-control-expose-headers', 'x-auth-token')
+    .send(_.pick(newUser, ['id', 'name', 'email']));
 });
 
 // getUser - Input jwt
-router.get("/me", auth, async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  const user = await User.findById(req.user._id).select("-password");
+router.get('/me', auth, async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const user = await User.findById(req.user._id).select('-password');
   res.send(user);
 });
 
 // addFavorite - Input JWT & req.body: plantObject
-router.put("/", auth, async (req, res) => {
+router.put('/', auth, async (req, res) => {
   // Validate Data
   const schema = Joi.object({
     plantId: Joi.number().required(),
@@ -81,14 +75,14 @@ router.put("/", auth, async (req, res) => {
 });
 
 // addNote - Input JWT & req.body: favoritesObject from state
-router.put("/notes", auth, async (req, res) => {
+router.put('/notes', auth, async (req, res) => {
   // Validate Data
   const schema = Joi.array().items(
     Joi.object({
       _id: Joi.required(),
       plantId: Joi.number().required(),
       common_name: Joi.string().max(255).required(),
-      notes: Joi.string().max(255).empty(""),
+      notes: Joi.string().max(255).empty(''),
       image: Joi.string().max(5000),
       plantUrl: Joi.string().max(5000),
     })
@@ -112,7 +106,7 @@ router.put("/notes", auth, async (req, res) => {
 });
 
 // deleteFavorite - Input JWT & req.body: plantMongoId
-router.delete("/", auth, async (req, res) => {
+router.delete('/', auth, async (req, res) => {
   // Validate Data
   const schema = Joi.object({
     plantMongoId: Joi.string().max(255).required(),
@@ -133,7 +127,7 @@ router.delete("/", auth, async (req, res) => {
 });
 
 // deleteAllFavorites Input via req.body: id
-router.delete("/favorites", auth, async (req, res) => {
+router.delete('/favorites', auth, async (req, res) => {
   // Validate DAta
   const schema = Joi.object({
     id: Joi.string().max(255).required(),
